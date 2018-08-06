c67ea5c40ddc551dd7f08ce2dbbe987f524fc3fc
Tatsuya Fujii <sekkatinakatatumuri@gmail.com>
2018-08-06 01:21:12 +0000

    user registration

--- config/app.php
+++ config/app.php
@@ -167,6 +167,8 @@ return [
         /*
          * Package Service Providers...
          */
+        // 追加
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -225,7 +227,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        // 追加
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
