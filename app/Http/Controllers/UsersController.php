be9da72b134810b0ec8f5c931a23ba11f70b9de5
Tatsuya Fujii <sekkatinakatatumuri@gmail.com>
2018-08-06 06:18:17 +0000

    follow, unfollow

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,11 +7,6 @@ use App\User;
 
 class UsersController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $users = User::paginate(10);
@@ -21,33 +16,6 @@ class UsersController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $user = User::find($id);
@@ -62,38 +30,34 @@ class UsersController extends Controller
 
         return view('users.show', $data);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    
+    public function followings($id)
     {
-        //
+        $user = User::find($id);
+        $followings = $user->followings()->paginate(10);
+
+        $data = [
+            'user' => $user,
+            'users' => $followings,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.followings', $data);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function followers($id)
     {
-        //
-    }
+        $user = User::find($id);
+        $followers = $user->followers()->paginate(10);
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $data = [
+            'user' => $user,
+            'users' => $followers,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.followers', $data);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -25,5 +25,15 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 // ログイン認証済み
 Route::group(['middleware' => 'auth'], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::group(['prefix' => 'users/{id}'], function () {
+        // xx番目のユーザーをフォローする
+        Route::post('follow', 'UserFollowController@store')->name('user.follow');
+        // xx番目のユーザーをアンフォローする
+        Route::delete('unfollow', 'UserFollowController@destroy')->name('user.unfollow');
+        // xx番目のユーザーがフォローしているユーザー一覧を表示する
+        Route::get('followings', 'UsersController@followings')->name('users.followings');
+        // xx番目のユーザーをフォローしているユーザー一覧を表示する
+        Route::get('followers', 'UsersController@followers')->name('users.followers');
+    });
     Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
